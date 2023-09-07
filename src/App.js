5f37b4a99017da75a15c0b5eb3ec03d5078b511e
Robert Negrete <74494424+PuttySmut@users.noreply.github.com>
2023-09-07 16:31:09 -0700

    updated app.css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import { Container, Navbar, NavbarBrand } from 'reactstrap';
 import NucampLogo from './app/assets/img/logo.png';
-//import logo from './logo.svg';
-//import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <Navbar dark color='primary' sticky='top' expand='md'>
+        <Navbar dark color='primary' sticky='top' expand='md'>
                <Container>
                     <NavbarBrand href='/'>
                         <img src={NucampLogo} alt='nucamp logo' />
                     </NavbarBrand>
-                </Container>
-               </Navbar>
-               I'm ready for the workshop!
+              </Container>
+        </Navbar>
+        I'm ready for the workshop!
     </div>
   );
 }
