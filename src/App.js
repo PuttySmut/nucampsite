f6ea310946e427a2f7e8ecbb098cbc09a027986f
Robert Negrete <74494424+PuttySmut@users.noreply.github.com>
2023-09-12 17:22:54 -0700

    campsites directory page and campsite detail

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
+
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-        <Navbar dark color='primary' sticky='top' expand='md'>
-               <Container>
-                    <NavbarBrand href='/'>
-                        <img src={NucampLogo} alt='nucamp logo' />
-                    </NavbarBrand>
-              </Container>
-        </Navbar>
-        I'm ready for the workshop!
+  <Header/>
+  <CampsitesDirectoryPage/>
+  <Footer/>      
+        
     </div>
   );
 }
